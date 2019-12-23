1a506aabe196e40b6580c36c7c0364f2ceaf7d18
Don Charles-Lambert <zcabdc0@ucl.ac.uk>
2019-12-23 02:27:35 +0000

    My First React Native App

---
+++ App.js
@@ -0,0 +1,73 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, ImageBackground, Image, Button, TextInput, Alert, TouchableOpacity } from 'react-native';
+
+export default class Login extends Component{
+  state = {
+    username: 'Username',
+    password: 'Password'
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <ImageBackground style = {styles.background} source={require('./img/bg.gif')}>
+        <Text style={{ color: "black", fontSize: 30 }}
+        >
+          MEDIC DRAW
+          </Text>
+
+        <Image style = {styles.login_image} source={require('./img/blankprofile.jpg')} resizeMode="contain"></Image>
+
+        <TextInput
+          style={styles.input}
+          value= {this.state.username}
+          onChangeText={(text) => this.setState({ username: text })}
+        />
+        <TextInput
+          style={styles.input}
+          value= {this.state.password}
+          onChangeText={(text) => this.setState({ password: text })}
+        />
+        <Button
+            title="Login"
+            onPress={() => Alert.alert("Welcome " + this.state.username)}
+          />
+        <Button
+            title="Sign Up"
+            onPress={() => Alert.alert("Let's get you an account!")}
+        />
+         
+        </ImageBackground>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  input: {
+    height: 40,
+    width: '60%',
+    borderColor: 'gray',
+    marginBottom: '2.5%',
+    borderWidth: 1
+  },
+
+  login_image: {
+    height: '20%',
+    marginBottom: '2.5%',
+  },
+
+  background: {
+    width: '100%',
+    height: '100%',
+    justifyContent: "center",
+    alignItems: "center",
+  }
+});
