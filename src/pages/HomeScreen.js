9381cca16a58dd7a70194de9e3e67c6892904d1e
xiaoqi tan <zcabxta@ucl.ac.uk>
2020-01-07 22:47:54 +0000

    error

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,12 +1,49 @@
 
 import * as React from 'react';
-import { StyleSheet, Text, View, ScrollView, Image, Button, TextInput, Alert, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, Button, TextInput, Form, AsyncStorage } from 'react-native';
+import { response } from 'express';
 
 export default class HomeScreen extends React.Component {
-    state = {
-            username: 'Username',
-            password: 'Password'
-          }
+    constructor(props) {
+      super(props);
+      this.state = {
+        username: '',
+        password: '',
+      }
+    }
+    
+    // componentDidMount() {
+    //   this._loadInitialState().done();
+    // }
+
+    // state = {
+    //         username: 'Username',
+    //         password: 'Password'
+    //       }
+
+    // constructor() {
+    //   super();
+    //   this.state = { user: {} };
+    //   this.onSubmit = this.handleSubmit.bind(this);
+    // }
+    // handleSubmit(e) {
+    //   e.preventDefault();
+    //   var self = this;
+    //   // On submit of the form, send a POST request with the data to the server.
+    //   fetch('http://localhost:5000', { 
+    //       method: 'POST',
+    //       data: {
+    //         Username: self.refs.Username,
+    //         Password: self.refs.Password
+    //       }
+    //     })
+    //     .then(function(response) {
+    //       //this is the line that is giving me the error
+    //       return response.json()
+    //     }).then(function(body) {
+    //       console.log(body);
+    //     });
+    //   }
 
     static navigationOptions = {
     title: 'Home',
@@ -16,26 +53,29 @@ export default class HomeScreen extends React.Component {
     return (
       <ScrollView style={{flex: 1}}>
         <View style={styles.container}>
-          {/* <ImageBackground style = {styles.background} source={require('./img/bg.gif')}> */}
           <Text style={{ color: "black", fontSize: 30 }}>MEDIC DRAW</Text>
-  
+
           <Image style = {styles.login_image} source={require('../../img/blankprofile.jpg')} resizeMode="contain"></Image>
   
           <TextInput
             style={styles.input}
-            placeholder= {this.state.username}
+            placeholder= 'username'
             autoCorrect={false}
             clearButtonMode="always"
-            onChangeText={(text) => this.setState({ username: text })}
-            password={true}
+            ref="Username"
+            onChangeText={username => this.setState({username})}
+            value={this.state.username}
             // secureTextEntry={true}
           />
           <TextInput
             style={styles.input}
-            placeholder= {this.state.password}
+            placeholder= "password"
             autoCorrect={false}
             clearButtonMode="always"
-            onChangeText={(text) => this.setState({ password: text })}
+            ref="Password"
+            onChangeText={password => this.setState({password})}
+            value={this.state.password}
+            password={true}
           />
           <Button
               title="Login"
@@ -43,14 +83,40 @@ export default class HomeScreen extends React.Component {
             />
           <Button
               title="Sign Up"
-              onPress={() =>this.props.navigation.navigate('CreateAccount')}
+              onPress= {() =>this.props.navigation.navigate('CreateAccount')}// {this.login} // {() =>this.props.navigation.navigate('CreateAccount')}
           />
            
-          {/* </ImageBackground> */}
         </View>
       </ScrollView>
       );
     }
+
+  //   login = () => {
+  //     alert(this.state.username);
+  //     fetch("https://89.36.71.180: 3000/users", {
+  //       method: "POST",
+  //       headers: {
+  //         'Accept': 'application/json',
+  //         'Content-type': 'application/json',
+  //       },
+  //       body: JSON.stringify({
+  //         username: this.state.username,
+  //         password: this.state.password,
+  //       })
+  //     })
+  //     .then((response) => response.json())
+  //     .then((res) => {
+  //       if (res.success === true) {
+  //         AsyncStorage.setItem('user', res.user);
+  //         this.props.navigation.navigate('Menu');
+  //       }
+  //       else {
+  //         alert(res.message);
+  //       }
+
+  //     })
+  //     .done();
+  //   }
   }
 
 
