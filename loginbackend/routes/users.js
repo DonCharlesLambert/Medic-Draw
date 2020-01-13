180f62245d7d6347ec4d16f3132ff3bd72d224b5
xiaoqi tan <zcabxta@ucl.ac.uk>
2020-01-13 11:26:19 +0000

    fixed backend error now frontend and backend are connected

--- loginbackend/routes/users.js
+++ loginbackend/routes/users.js
@@ -2,34 +2,56 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 
-// var connection = mysql.createConnection({
-//   // host:'localhost',
-//   // user: 'root',
-//   // password: 'root',
-//   // database: 'users'
-//   authentication: {
-//     options: {
-//       userName: "sylvia", // update me
-//       password: "Team30medicaldrawing" // update me
-//     },
-//     type: "default"
-//   },
-//   server: "team-30-medical-drawings.database.windows.net", // update me
-//   options: {
-//     database: "medical-drawings", //update me
-//     encrypt: true 
-//   }
-// });
+var connection = mysql.createConnection({
+  // host:'localhost',
+  // user: 'root',
+  // password: 'root',
+  // database: 'users'
+  authentication: {
+    options: {
+      userName: "sylvia", // update me
+      password: "Team30medicaldrawing" // update me
+    },
+    type: "default"
+  },
+  server: "team-30-medical-drawings.database.windows.net", // update me
+  options: {
+    database: "medical-drawings", //update me
+    encrypt: true 
+  }
+});
 
+router.get('/', function(req, res, next) {
+  console.log('1234');
+  res.send({message: 'get method users'});
+});
 
 router.post('/', function(req, res, next) {
   console.log('backend');
-  res.send({message: 'Testing to get data from backend'});
-});
+  res.send({message: req.body.username});
 
-router.post('/users', function(req, res, next) {
-  console.log('backend');
-  res.send({message: 'Testing to get data from backend'});
+  var username = req.body.username;
+  var password = req.body.password;
+
+
+connection.query(
+  "SELECT * FROM users WHERE username = ? AND password = ? ", 
+  // "INSERT INTO users (username, password) values (sylvia test)",
+  [username, password], function(err, row, field) {
+    console.log('connected');
+    if (err) {
+      res.send ({'success': false, 'message': 'could not connect to database' });
+    }
+
+    if (row.length > 0) {
+      console.log('found');
+      res.send ({'success': true, 'user': row[0].username});
+    }
+    else {
+      console.log('not found');
+      res.send ({'success': false, 'message': 'user not found'});
+    }
+  });
 });
 
 module.exports = router;
