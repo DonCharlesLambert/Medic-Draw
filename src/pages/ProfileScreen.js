98a6caf0b18d4c4717e6618f7c6882df3d18aa43
xiaoqi tan <zcabxta@ucl.ac.uk>
2020-01-13 23:09:14 +0000

    added frontend

---
+++ src/pages/ProfileScreen.js
@@ -0,0 +1,74 @@
+import * as React from 'react';
+import {Image, View, Button, Text, StyleSheet } from 'react-native';
+import { Header } from 'react-native-elements';
+
+
+export default class ProfileScreen extends React.Component {
+
+  static navigationOptions = {
+    headerShown: false
+    };
+
+    Exported = () => {
+        this.props.navigation.navigate('Menu');
+    }
+
+    render() {
+        return (
+            <View>
+              <Header
+                leftComponent={{ icon: 'menu', color: '#fff' }}
+                centerComponent={{ text: 'Profile', style: {fontSize: 20, color: '#fff' } }}
+                rightComponent={{ icon: 'home', color: '#fff' }}
+                />
+              <Image style = {styles.image} source={require('../../img/doctor_profile.jpg')} resizeMode="contain"></Image>
+
+              <Text style = {styles.textInput}>Doctor: nameOfDoctor</Text>
+              <Text style = {styles.textInput}>Doctor Number: 12345678</Text>
+              <Text style = {styles.textInput}>Patient Number: 39</Text>
+              <Button
+            title="Create New Patient"
+            onPress={() => this.props.navigation.navigate('CreateNewPatient')}
+          />
+
+          <Button
+            title="Edit Existing Patient"
+            onPress={() => this.props.navigation.navigate('CreateNewPatient')}
+          />
+
+          <Button
+            title="View UICC Version"
+            onPress={() => this.props.navigation.navigate('Result')}
+          />
+            </View>
+          );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: 30,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    textInput: {
+      marginLeft: '5%',
+      marginBottom: '2%',
+    },
+    image: {
+      padding: 100,
+      height: '30%',
+      marginTop: '5%',
+      marginBottom: '10%',
+    },
+  
+    // background: {
+    //   width: '100%',
+    //   height: '100%',
+    //   justifyContent: "center",
+    //   alignItems: "center",
+    // }
+  });
+  
--- src/pages/SelectVersionScreen.js
+++ src/pages/SelectVersionScreen.js
@@ -11,8 +11,7 @@ export default class SelectVersionScreen extends React.Component {
     render() {
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-around', height: 300}}>
-              {/* bainuo please work on this page */}
-              <Text>Slect UICC version</Text>
+              
               <Button
                 title="OK"
                 onPress={() => this.props.navigation.navigate('Result')}
