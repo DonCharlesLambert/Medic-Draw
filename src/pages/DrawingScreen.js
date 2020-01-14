6eccc7b89938ee019141e55579b1a6b0398d0e05
Don Charles-Lambert <zcabdc0@ucl.ac.uk>
2020-01-14 15:53:58 +0000

    Draw

--- src/pages/DrawingScreen.js
+++ src/pages/DrawingScreen.js
@@ -1,16 +1,35 @@
 import * as React from 'react';
-import { View, Button } from 'react-native';
+import { Button, View } from 'react-native';
+import Point from './Misc/Point';
 
 export default class drawingScreen extends React.Component {
+    constructor() {
+        super();
+        this.state = { points: [], colour: '#000' }
+    }
+
+    onTouchEvent(name, ev) {
+        let jim = new Point(ev.nativeEvent.locationX, ev.nativeEvent.locationY, this.state.colour, this.state.points.length);
+        this.state.points.push(jim);
+        this.forceUpdate();
+    }
+
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-around', height: 300}}>
-              {/* don please continue working on this page */}
-              <Button
-                title="Don is here!"
-                onPress={() => this.props.navigation.navigate('Question')}
-              />
+            <View>
+              <View style={{alignItems: 'center', justifyContent: 'space-around', height: '4%'}}>
+
+              </View>
+              <View style={{alignItems: 'center', justifyContent: 'space-around', height: '92%'}}
+                    onStartShouldSetResponder={(ev) => true}
+                    onResponderGrant={this.onTouchEvent.bind(this, "Touch")}
+                    onResponderMove={this.onTouchEvent.bind(this, "Move")}>
+                  {this.state.points.map(ele => ele.render())}
+              </View>
+              <View style = {{height: '4%'}}>
+                    <Button title="Finish" onPress={() => this.props.navigation.navigate('Question')}/>
+              </View>
             </View>
-          );
+        );
     }
 }
