8419f2f6f2bc2d06aa51bfb8076e2696d67bdbd4
Don Charles-Lambert <zcabdc0@ucl.ac.uk>
2020-01-14 21:47:38 +0000

    Better login screen 50% done

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,7 +1,8 @@
 
 import * as React from 'react';
-import { StyleSheet, Text, View, ScrollView, Image, Button, TextInput, Alert, AsyncStorage } from 'react-native';
+import { StyleSheet, ImageBackground, Text, View, TouchableOpacity, ScrollView, Image, Button, TextInput, Alert, AsyncStorage } from 'react-native';
 import { Header } from 'react-native-elements';
+import TouchableHighlight from "react-native-web/dist/exports/TouchableHighlight";
 
 export default class HomeScreen extends React.Component {
 
@@ -14,163 +15,93 @@ export default class HomeScreen extends React.Component {
     }
   }
 
-    componentDidMount() {
-      this._loadInitialState().done();
-    }
-
-    _loadInitialState = async () => {
-      var value = await AsyncStorage.getItem('users');
-      if (value !== null) {
-        this.props.navigate('Menu');
-      }
-    }
-
-    static navigationOptions = {
-      headerShown: false
-      };
-
-    login = () => {
-      if (this.state.username === '') {
-        Alert.alert('Username cannot be empty!');
-        return;
-      }
-
-      if (this.state.password === '') {
-        Alert.alert('Password cannot be empty!');
-        return;
-      }
-
-      this.props.navigation.navigate('Profile');
-
-      // fetch('http://facebook.github.io/react-native/movies.json')
-      // .then((response) => response.json())
-      // .then((responseJson) => {
-      //   alert("s")
-      //   return responseJson.movies;
-      // })
-      // .catch((error) => {
-      //   console.error(error);
-      // });
-
-      // alert(this.state.username);
-
-      // fetch('http://127.0.0.1:3000/users', {
-      //   method: 'POST',
-      //   headers: {
-      //     'Accept': 'application/json',
-      //     'Content-type': 'application/json',
-      //   },
-      //   body: JSON.stringify({
-      //     username: this.state.username,
-      //     password: this.state.password,
-      //   })
-      // })   
-      // .then((response) => response.json()) 
-      // .then((res) => {   
-      //   alert(res.message)     
-      //   if (res.success === true) {
-      //     AsyncStorage.setItem('user', res.user);
-      //     this.props.navigation.navigate('Menu');
-      //   }
-      //   else {
-      //     alert(res.message);
-      //   }
-      // })
-      // .catch((err) => {
-      //   console.error(err);
-      // })
-      // .done();
-    }
-
-    render() {
-    return (
-      <ScrollView style={{flex: 1}}>
-        <Header centerComponent={{ text: 'MEDICAL DRAW', style: { fontSize: 20, color: '#fff' } }} />
-        <View style={styles.container}>
-          <Text style={{ color: "black", fontSize: 20 }}>WELCOME!</Text>  
-          <TextInput
-            style={styles.input}
-            placeholder= {"username"}
-            autoCorrect={false}
-            clearButtonMode="always"
-            ref= "username"
-            onChangeText={(username) => this.setState({username})}
-            value={this.state.username}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder= {"password"}
-            autoCorrect={false}
-            clearButtonMode="always"
-            ref= "password"
-            onChangeText={(password) => this.setState({password})}
-            value={this.state.password}
-            secureTextEntry = {true}
-          />
-          <Button
-              title="Login"
-              onPress= {this.login.bind(this)} 
-              // onPress={() =>this.props.navigation.navigate('Profile')}
-            />
-          <Button
-              title="Do not have an account? Sign Up!"
-              onPress= {() =>this.props.navigation.navigate('CreateAccount')}// {this.login} // {() =>this.props.navigation.navigate('CreateAccount')}
-          />
-
-           
-        </View>
-      </ScrollView>
-      );
-    }
-
-  //   login = () => {
-  //     alert(this.state.username);
-  //     fetch("https://89.36.71.180: 3000/users", {
-  //       method: "POST",
-  //       headers: {
-  //         'Accept': 'application/json',
-  //         'Content-type': 'application/json',
-  //       },
-  //       body: JSON.stringify({
-  //         username: this.state.username,
-  //         password: this.state.password,
-  //       })
-  //     })
-  //     .then((response) => response.json())
-  //     .then((res) => {
-  //       if (res.success === true) {
-  //         AsyncStorage.setItem('user', res.user);
-  //         this.props.navigation.navigate('Menu');
-  //       }
-  //       else {
-  //         alert(res.message);
-  //       }
-
-  //     })
-  //     .done();
-  //   }
+  componentDidMount() {
+    this._loadInitialState().done();
   }
+
+  _loadInitialState = async () => {
+    var value = await AsyncStorage.getItem('users');
+    if (value !== null) {
+      this.props.navigate('Menu');
+    }
+  }
+
+  static navigationOptions = {
+    headerShown: false
+  };
+
+  login = () => {
+    if (this.state.username === '') {
+      Alert.alert('Username cannot be empty!');
+      return;
+    }
+
+    if (this.state.password === '') {
+      Alert.alert('Password cannot be empty!');
+      return;
+    }
+
+    this.props.navigation.navigate('Profile');
+  }
+
+  render() {
+    return (
+        <View>
+          <Header centerComponent={{text: 'MEDIC DRAW', style: {top: 0, fontSize: 20, color: '#fff'}}}/>
+          <ImageBackground source={require('../../img/background.jpg')} style={styles.container}>
+            <Text style={{color: "black", fontSize: 20}}>WELCOME!</Text>
+              <TextInput
+                  style={styles.input}
+                  placeholder={"username"}
+                  autoCorrect={false}
+                  clearButtonMode="always"
+                  ref="username"
+                  onChangeText={(username) => this.setState({username})}
+                  value={this.state.username}
+              />
+              <TextInput
+                  style={styles.input}
+                  placeholder={"password"}
+                  autoCorrect={false}
+                  clearButtonMode="always"
+                  ref="password"
+                  onChangeText={(password) => this.setState({password})}
+                  value={this.state.password}
+                  secureTextEntry={true}
+              />
+            <Button
+                title="Login"
+                onPress={this.login.bind(this)}
+                // onPress={() =>this.props.navigation.navigate('Profile')}
+            />
+            <Button
+                title="Do not have an account? Sign Up!"
+                onPress={() => this.props.navigation.navigate('CreateAccount')}// {this.login} // {() =>this.props.navigation.navigate('CreateAccount')}
+            />
+          </ImageBackground>
+        </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    marginTop: 30,
     backgroundColor: '#fff',
+    height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
   },
 
   input: {
     height: 40,
-    width: '60%',
-    borderColor: 'gray',
+    width: '70%',
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
     marginBottom: '2.5%',
-    borderWidth: 1
+    borderRadius: 10,
+    padding: 15
   },
-  // background: {
-  //   width: '100%',
-  //   height: '100%',
-  //   justifyContent: "center",
-  //   alignItems: "center",
-  // }
+
+  button: {
+    width: '70%',
+  }
 });
