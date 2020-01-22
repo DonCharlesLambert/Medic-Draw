0d401d011678291a4e84c0f276a370f657d92e80
Don Charles-Lambert <zcabdc0@ucl.ac.uk>
2020-01-22 22:37:13 +0000

    new results screen on the way

--- src/pages/ResultScreen.js
+++ src/pages/ResultScreen.js
@@ -1,8 +1,16 @@
 import * as React from 'react';
 import {StyleSheet, View, Button, Text, Image, Alert } from 'react-native';
+import ModalDropdown from "react-native-modal-dropdown";
 
 
 export default class ResultScreen extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          UICCVersion: 'UICC Version 8',
+          UICCOptions: ['UICC Version 8', 'UICC Version 7', 'UICC Version 6','UICC Version 5','UICC Version 4',],
+      }
+  }
 
   static navigationOptions = {
     title: 'Result',
@@ -15,35 +23,14 @@ export default class ResultScreen extends React.Component {
 
     render() {
         return (
-            <View>
-              <Text style= {styles.textInput}>Patient Name: Name</Text>
-              <Text style= {styles.textInput}>Patient Age: 63</Text>
-              <Text style= {styles.textInput}>Tumour Area: Upper side of larynx</Text>
-              <Text style= {styles.textInput}>Patient Age: 63</Text>
-              <Text style= {styles.textInputBold}>According to UICC version 8, the tumour level is T3:</Text>
-              <Text style= {styles.textInputBold}>Tumour more than 4cm in or extension to lingual surface of epiglottis. </Text>
-
-              <Image style = {styles.image} source={require('../../img/larynx.png')} resizeMode="contain"></Image>
-                <Button
-                  title="Change UICC version"
-                  onPress={() => Alert.alert('UICC VERSION','Current Version 8',[
-                    {text:"1", onPress:this.opntion1Selected},
-                    {text:"2", onPress:this.opntion2Selected},
-                    {text:"3", onPress:this.opntion3Selected},
-                    {text:"4", onPress:this.opntion4Selected},
-                    {text:"5", onPress:this.opntion5Selected},
-                    {text:"6", onPress:this.opntion6Selected},
-                    {text:"7", onPress:this.opntion7Selected},
-                    {text:"8", onPress:this.opntion8Selected},
-
-
-
-                  ]
-                )}
-                />
-                <Button
-                  title="Export As PDF"
-                  onPress={this.Exported}
+            <View style = {{justifyContent: 'center', alignItems: 'center'}}>
+                <ModalDropdown
+                    style = {styles.button}
+                    onSelect = {this.state.UICCVersion}
+                    options={this.state.UICCOptions}
+                    defaultValue = {'Change Version: UICC Version 8'}
+                    color = {'#bde0eb'}
+                    onSelect = {(version) => this.setState({UICCVersion: (String(this.state.UICCOptions[version]))})}
                 />
             </View>
           );
@@ -77,6 +64,14 @@ const styles = StyleSheet.create({
     height: '40%',
     marginTop: '5%',
     marginBottom: '10%',
-    // marginRight:'500%',
+  },
+  button: {
+     width: '50%',
+     backgroundColor: '#bde0eb',
+     alignItems: 'center',
+     justifyContent: "center",
+     margin: 5,
+     marginTop: 20,
+     borderRadius: 10,
   },
 });
